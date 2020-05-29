6bed0062780450c9b9878ebb8bc880e27515c5ea
swapnil <swapnilbhosale8222@gmail.com>
2020-05-29 08:07:53 +0530

    Initial Commit

---
+++ Test Inceptive Technologies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test Inceptive Technologies
+//
+//  Created by Mac on 29/05/20.
+//  Copyright © 2020 Mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
